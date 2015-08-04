c37f63bc60e6ebf269965a9a77fbab5fca1a4aba
Matt Buland <gik0geck0@gmail.com>
2015-08-04 07:02:51 -0600

    Bug fixes and code review changes

--- midibridge.sh
+++ midibridge.sh
@@ -2,13 +2,13 @@
 
 # Look up the names / ports of USB-MIDI and Alesis then connect Alesis > USB-MIDI
 if [ "$1" = "start" ]; then
-    usb=$(aconnect -i | sed 's/://' | awk "BEGIN {dev=0;} /client [0-9]+ 'USB2.0-MIDI/ {dev=\$2;} END {print dev}")
-    alesis=$(aconnect -i | sed 's/://' | awk "BEGIN {dev=0;} /client [0-9]+ 'Alesis/ {dev=\$2;} END {print dev}")
+    usb=$(aconnect -i | sed 's/://' | awk "BEGIN {dev=0;} /client [0-9]+ 'USB2.0-MIDI/ {dev=\$2;} END {print dev;}")
+    alesis=$(aconnect -i | sed 's/://' | awk "BEGIN {dev=0;} /client [0-9]+ 'Alesis/ {dev=\$2;} END {print dev;}")
     
-    if [ ! "$alesis" = "0" ] && [ ! "$usb" = "0" ]; then
-        echo "aconnect $alesis $usb"
+    if [[ "$alesis" -ne "0" && "$usb" -ne "0" ]]; then
+        aconnect $alesis $usb
     else
-        if [ "$alesis" = "0" ]; then
+        if [[ "$alesis" -eq "0" ]]; then
             echo "Cannot find Alesis drum kit"
         else
             echo "Cannot find USB-MIDI"
