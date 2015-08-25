52cbe762b7f9607547a182a9bf7033fd2f655fc5
Matt Buland <gik0geck0@gmail.com>
2015-08-25 03:09:17 -0600

    Default to start and fix if condition

--- midibridge.sh
+++ midibridge.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Look up the names / ports of USB-MIDI and Alesis then connect Alesis > USB-MIDI
-if [ "$1" = "start" ]; then
+if [ "$1" = "start" ] || [ -z "$1" ]; then
     usb=$(aconnect -i | sed 's/://' | awk "BEGIN {dev=0;} /client [0-9]+ 'USB2.0-MIDI/ {dev=\$2;} END {print dev;}")
     alesis=$(aconnect -i | sed 's/://' | awk "BEGIN {dev=0;} /client [0-9]+ 'Alesis/ {dev=\$2;} END {print dev;}")
     
-    if [[ "$alesis" -ne "0" && "$usb" -ne "0" ]]; then
+    if [ "$alesis" -ne "0" ] && [ "$usb" -ne "0" ]; then
+        aconnect -i
+        echo "aconnect $alesis $usb"
         aconnect $alesis $usb
     else
         if [[ "$alesis" -eq "0" ]]; then
